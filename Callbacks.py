f0cd07ecc2c9868a9a4e78b7ff80254c4e871109
brendlin <kurt.brendlinger@cern.ch>
2021-03-21 01:27:00 +0100

    Multiple angular turbine indicator thingies

--- Callbacks.py
+++ Callbacks.py
@@ -5,21 +5,26 @@ from dash.exceptions import PreventUpdate
 
 from app import app
 
+NTURBINES=4
+
 # Update the wind and turbine angles
-@app.callback([Output('turbine-img', 'style'),
-               Output('wind-arrow-img', 'style'),
-               ],
+@app.callback(list(Output('turbine-%d-img'%(i), 'style') for i in range(NTURBINES)) +
+              list(Output('wind-arrow-%d-img'%(i), 'style') for i in range(NTURBINES)),
               Input('interval-component', 'n_intervals'),
-              [State('turbine-img', 'style'),
-              State('wind-arrow-img', 'style'),
-              ]
+              list(State('turbine-%d-img'%(i), 'style') for i in range(NTURBINES)) +
+              list(State('wind-arrow-%d-img'%(i), 'style') for i in range(NTURBINES)),
               )
-def UpdateEverything(n_intervals,turbine_style,wind_style) :
-    current_windloc = int(wind_style['transform'].split('(')[1].split('deg')[0])
-    current_windloc += int(np.random.normal(0,10))
-    current_turbloc = int(current_windloc + np.random.normal(0,5))
+def UpdateEverything(n_intervals,*styles) :
 
-    wind_style['transform'] = 'rotate({}deg)'.format(current_windloc)
-    turbine_style['transform'] = 'rotate({}deg)'.format(current_turbloc)
+    for i in range(NTURBINES) :
+        current_windloc = int(styles[i+NTURBINES]['transform'].split('(')[1].split('deg')[0])
+        current_windloc += int(np.random.normal(0,10))
+        current_turbloc = int(current_windloc + np.random.normal(0,5))
 
-    return turbine_style,wind_style
+        # wind loc
+        styles[i+NTURBINES]['transform'] = 'rotate({}deg)'.format(current_windloc)
+
+        # turbine loc
+        styles[i]['transform'] = 'rotate({}deg)'.format(current_turbloc)
+
+    return styles
--- Layout.py
+++ Layout.py
@@ -13,28 +13,37 @@ import pandas as pd
 from app import app
 from .Callbacks import *
 
+NTURBINES=4
+
 storage = [
     dcc.Interval(id='interval-component',n_intervals=0,interval=1*3000), # in milliseconds
 ]
 
+turbine_style = style={'border-radius':'50%','position':'relative','left':'0px'}
+wind_style={'border-radius':'50%','position':'absolute','left':'0px','transform':'rotate(0deg)'}
+
+# Make the angular position indicators
+wind_turbine_divs = []
+for i in range(NTURBINES) :
+    tmp = html.Div(children=[html.Img(id='turbine-%d-img'%(i),height='150px',width='150px',style=turbine_style,src='assets/turbine.png'),
+                             html.Img(id='wind-arrow-%d-img'%(i),height='150px',width='150px',style=wind_style,src='assets/wind.png'),
+                             ],
+                   style={'display':'inline-block','position':'relative'}
+                   )
+    wind_turbine_divs.append(tmp)
+    if i+1 < NTURBINES :
+        wind_turbine_divs.append(html.Div(style={'width':'10px','display':'inline-block'}))
+
+
+# main layout
 layout = html.Div( # Main Div
     children=[ # Main Div children
         html.Div( # Banner
             children=[
-                html.H5(children='Hello world!'),
-                html.Div(children=[html.Img(id='turbine-img',height='150px',width='150px',
-                                            style={'border-radius':'50%',
-                                                   'position':'relative','left':'0px'},
-                                            src='assets/turbine.png'),
-                                   html.Img(id='wind-arrow-img',height='150px',width='150px',
-                                            style={'border-radius':'50%',
-                                                   'position':'absolute','left':'8px',
-                                                   'transform':'rotate(0deg)'},
-                                            src='assets/wind.png'),
-                                   ],
-                         style={},
-                         ),
+                html.H5(children='Wind Turbine Predictive Maintenance'),
+                *wind_turbine_divs,
             ],
+            style={},
         ), # Banner End
         *storage,
     ], # Main Div children End
